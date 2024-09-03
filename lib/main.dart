04a0d820f3741a3d677e50b343a358e670f5f11d
Asher Usmani <asherahmed900@gmail.com>
2024-09-04 04:11:43 +0500

    completed

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage_ui/pages/Homepage.dart';
+
+void main() =>runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
